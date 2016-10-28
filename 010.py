cdaa0dd41c95a2db75b243a95ba7d8cfcc9e1ae6
MaxMontoro <samubalogh@gmail.com>
2016-10-28 11:39:58 +0200

    refactor

--- 010.py
+++ 010.py
@@ -8,12 +8,14 @@ Find the sum of all the primes below two million.
 
 from itertools import count
 from math import sqrt
+from timeit import Timer
+
             
-def prime_generator_under(n):
+def prime_generator_below(n):
     yield 2
-    for num in count(3):
+    for num in count(3,2):
         prime = True
-        for divisor in range(int(sqrt(num)), 1,-1):
+        for divisor in range(int(sqrt(num)), 2,-1):
             if num % divisor == 0:
                 prime = False
                 break
@@ -22,29 +24,13 @@ def prime_generator_under(n):
                 yield num
             else:
                 return
-            
-summa = 0
-first_100_primes = []
-for i in nth_prime_generator(100):
-    first_100_primes.append(i)
-    summa += i
-    
-# print(first_100_primes)
 
 def sum_of_primes_below(n):
     summa = 0
-    for prime in prime_generator_under(n):
+    for prime in prime_generator_below(n):
         summa += prime
     yield summa
 
-counti = 0
-summa = 0
-for i in prime_generator_under(2000000):
-    counti += 1
-    summa += i
-print(counti)
-print("sum: ", summa)
-    
-for i in sum_of_primes_below(100):
+for i in sum_of_primes_below(2000000):
     print(i)
     
